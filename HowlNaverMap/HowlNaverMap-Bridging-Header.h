530698b09eaf55a9b27bc4f8360b8098fd9b6453
MyeongSic <you6878@icloud.com>
2017-06-03 00:41:47 +0900

    init

---
+++ HowlNaverMap/HowlNaverMap-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  HowlNaverMap-Bridging-Header.h
+//  HowlNaverMap
+//
+//  Created by 유명식 on 2017. 6. 3..
+//  Copyright © 2017년 swift. All rights reserved.
+//
+
+#ifndef HowlNaverMap_Bridging_Header_h
+#define HowlNaverMap_Bridging_Header_h
+
+
+#import <NMapViewerSDK/NMapView.h>
+#import <NMapViewerSDK/NMapLocationManager.h>
+
+
+#endif /* HowlNaverMap_Bridging_Header_h */
--- HowlNaverMap/ViewController.swift
+++ HowlNaverMap/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,NMapViewDelegate,NMapPOIdataOverlayDelegate {
 
+    
+    var mapView: NMapView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView = NMapView(frame: self.view.frame)
+        self.navigationController?.navigationBar.isTranslucent = false
+        
+        if let mapView = mapView {
+            // set the delegate for map view
+            mapView.delegate = self
+            
+            // set the application api key for Open MapViewer Library
+            mapView.setClientId("Yr7SSw2IobTERk_US6Om")
+                mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                
+                view.addSubview(mapView)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func onMapView(_ mapView: NMapView!, initHandler error: NMapError!) {
+        if (error == nil) { // success
+            // set map center and level
+            mapView.setMapCenter(NGeoPoint(longitude:126.978371, latitude:37.5666091), atLevel:11)
+            
+            // set for retina display
+            mapView.setMapEnlarged(true, mapHD: true)
+        } else { // fail
+            print("onMapView:initHandler: \(error.description)")
+        }
     }
-
+    
+    
+    
+    
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForOverlayItem poiItem: NMapPOIitem!, selected: Bool) -> UIImage! {
+        return nil
+    }
+    
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, anchorPointWithType poiFlagType: NMapPOIflagType) -> CGPoint {
+        return CGPoint(x: 0, y: 0)
+    }
+    
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, calloutOffsetWithType poiFlagType: NMapPOIflagType) -> CGPoint {
+        return CGPoint(x: 0, y: 0)
+    }
+    
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForCalloutOverlayItem poiItem: NMapPOIitem!, constraintSize: CGSize, selected: Bool, imageForCalloutRightAccessory: UIImage!, calloutPosition: UnsafeMutablePointer<CGPoint>!, calloutHit calloutHitRect: UnsafeMutablePointer<CGRect>!) -> UIImage! {
+        return nil
+    }
 
 }
 
